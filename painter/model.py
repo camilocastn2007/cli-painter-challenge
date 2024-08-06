c7a8843dcc460da56f8473324ed9b8c337d3b3db
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-08-06 11:27:04 -0500

    point circle

--- painter/model.py
+++ painter/model.py
@@ -1,5 +1,13 @@
-# TODO: Add code here
-class point
-    def __init__(self,x:float,y:float)
-        self.x: float: x
-        self.y: float: y
+class Point
+    def __init__(self, x: float, y: float):
+        self.x: float: 0,0
+        self.y: float: 0,0
+
+class Circle
+    def __init__(self,center:Point,radius:float ):
+        self.center: Point: center
+        self.radius: float:  0,0
+
+    def area(self) ->float:
+        math.pi(self.radius)**2
+        return area
